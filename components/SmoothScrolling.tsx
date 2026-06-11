f8bd1f139dd2019ae5798de8835ab9b861bb86a9
diialex <alexromeroginer@gmail.com>
2026-06-11 02:52:48 +0200

    quick lenis fix

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -11,7 +11,7 @@ export default function SmoothScrolling({ children }: { children: React.ReactNod
 
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: 'vertical',
       gestureDirection: 'vertical',
       smooth: true,
